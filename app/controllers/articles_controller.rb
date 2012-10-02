034f3f205456688cdbb011f1da664153dacae054
wfsiew <siewwingfei@hotmail.com>
2012-10-02 18:17:05 +0800

    modified to support i18n

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -45,7 +45,7 @@ class ArticlesController < ApplicationController
 
 		respond_to do |format|
 			if @article.save
-				format.html { redirect_to @article, :notice => 'Article was successfully created.' }
+				format.html { redirect_to @article, :notice => t('articles.create_success') }
 				format.json { render :json => @article, :status => :created, :location => @article }
 			else
 				format.html { render :action => "new" }
@@ -61,7 +61,7 @@ class ArticlesController < ApplicationController
 
 		respond_to do |format|
 			if @article.update_attributes(params[:article])
-				format.html { redirect_to @article, :notice => 'Article was successfully updated.' }
+				format.html { redirect_to @article, :notice => t('articles.update_success') }
 				format.json { head :ok }
 			else
 				format.html { render :action => "edit" }
@@ -85,6 +85,6 @@ class ArticlesController < ApplicationController
 	def notify_friend
 		@article = Article.find(params[:id])
 		Notifier.email_friend(@article, params[:name], params[:email]).deliver
-		redirect_to @article, :notice => 'Successfully sent a message to your friend'
+		redirect_to @article, :notice => t('articles.notify_friend_success')
 	end
 end
